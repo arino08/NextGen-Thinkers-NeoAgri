991be60fff3b7a6f09b14425a0333f319cbebe4a
Ariz <arinopc22@gmail.com>
2026-04-03 23:15:51 +0530

    [A2] feat(db): create drone marker CRUD functions

---
+++ db/markers.js
@@ -0,0 +1,29 @@
+import { db } from './schema';
+
+export async function insertMarker(marker) {
+  const query = `
+    INSERT OR REPLACE INTO drone_markers (capture_id, latitude, longitude, disease, confidence, timestamp, synced)
+    VALUES (?, ?, ?, ?, ?, ?, ?);
+  `;
+  await db.runAsync(query, [
+    marker.capture_id,
+    marker.latitude,
+    marker.longitude,
+    marker.disease,
+    marker.confidence,
+    marker.timestamp,
+    marker.synced !== undefined ? marker.synced : 1
+  ]);
+}
+
+export async function getMarkerById(capture_id) {
+  return await db.getFirstAsync('SELECT * FROM drone_markers WHERE capture_id = ?;', [capture_id]);
+}
+
+export async function getAllMarkers() {
+  return await db.getAllAsync('SELECT * FROM drone_markers;');
+}
+
+export async function clearMarkers() {
+  await db.runAsync('DELETE FROM drone_markers;');
+}

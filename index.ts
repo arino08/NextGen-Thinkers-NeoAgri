5c35fb07ed23a97ad9f6232b3357740c4a51853c
Arshan18 <141809406+Arshan18@users.noreply.github.com>
2026-04-04 16:17:21 +0530

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App)
-// It ensures the app works in both Expo Go and native builds
-registerRootComponent(App);
